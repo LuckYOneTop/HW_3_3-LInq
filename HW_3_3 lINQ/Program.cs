fda11c68cff50371e69a74b1f2aa801c1b2a7099
LuckYOneTop <124349839+LuckYOneTop@users.noreply.github.com>
2023-03-13 14:46:29 +0200

    Linq

---
+++ HW_3_3 lINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_3_3_lINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
